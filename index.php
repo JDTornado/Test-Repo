dd75a4037f3b6105a212c18b7abe1f7d42c55c45
Josh <Josh@Tornado>
2017-07-01 13:35:49 -0700

    Updates for setting up a sample display

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Malibu Seafood</title>
+        <style>
+            @font-face {
+                font-family: 'American Typewriter';
+                src: url('/styles/fonts/AmericanTypewriter/AmericanTypewriter.eot');
+                src: url('/styles/fonts/AmericanTypewriter/AmericanTypewriter.eot?#iefix') format('embedded-opentype'),
+                        url('/styles/fonts/AmericanTypewriter/AmericanTypewriter.woff2') format('woff2'),
+                        url('/styles/fonts/AmericanTypewriter/AmericanTypewriter.woff') format('woff'),
+                        url('/styles/fonts/AmericanTypewriter/AmericanTypewriter.ttf') format('truetype'),
+                        url('/styles/fonts/AmericanTypewriter/AmericanTypewriter.svg#AmericanTypewriter') format('svg');
+                font-weight: normal;
+                font-style: normal;
+            }
+        </style>
+    </head>
+    <body style="background-image: url('/images/WoodBkgrnd.jpg'); background-repeat: repeat; background-attachment: fixed; text-align: center; margin: 0px;">
+        <div style="position: relative; width: 100%; background-image: url('/images/WaveBkGrnd.gif'); background-repeat: repeat; margin-top: 7%;">
+            <div style="position: relative; width: 100%; max-width: 1336px; min-width: 336px; max-height: 660px; padding: 1.5%; background-image: url('/images/Panel1BkGrnd.gif'); background-size: 100% 100%; background-repeat: no-repeat; background-position: center center; margin-left: auto; margin-right: auto;">
+                <div style="position: relative; width: 100%; text-align: left;">
+                    <img src="/images/MSlogo1.gif" style="margin: 0px; width: 30%; float: left;" alt="" />
+                    <div style="font-family: 'American Typewriter'; font-size: 1.4em; height: 660px;">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent laoreet odio eu dui condimentum, sed vestibulum enim dictum. Cras faucibus, dui sed ullamcorper euismod, lorem elit aliquet augue, et placerat mi metus nec justo. Aenean sit amet sagittis lacus. Etiam scelerisque ac eros non cursus. Vestibulum pharetra ipsum vel eros placerat, vel ullamcorper quam ullamcorper. Cras odio sapien, consequat sit amet mauris eu, rhoncus accumsan augue. Etiam finibus sodales tempus. Nullam lobortis tempus tellus, eu placerat velit finibus sit amet. Curabitur scelerisque nec justo in fermentum.
+                        <br /><br />
+                        In felis nisl, bibendum id dapibus in, luctus vitae ipsum. Maecenas bibendum at dolor euismod ullamcorper. Nullam consectetur metus non tortor congue, sed congue turpis interdum. Mauris elementum nunc nec odio gravida molestie. Maecenas porttitor pellentesque nibh non scelerisque. Suspendisse potenti. Praesent gravida ornare nisi, non pellentesque elit suscipit non. Mauris quis orci at nibh viverra posuere dictum dictum quam. Etiam lobortis ultricies facilisis. Quisque pellentesque lectus nulla, at vulputate nunc posuere sit amet. Curabitur vulputate sapien sed turpis pharetra efficitur. Proin nec dapibus nulla. Quisque pellentesque lectus nulla, at vulputate nunc posuere sit amet. Curabitur vulputate sapien sed turpis pharetra efficitur. Proin nec dapibus nulla.
+                    </div>
+                    <div style="clear: both;"></div>
+                </div>
+            </div>
+        </div>
+        <div style="position: relative; width: 100%; background-image: url('/images/WaveBkGrnd.gif'); background-repeat: repeat; margin-top: 7%;">
+            <div style="position: relative; width: 100%; max-width: 1336px; min-width: 336px; max-height: 660px; padding: 1.5%; background-image: url('/images/Panel1BkGrnd.gif'); background-size: 100% 100%; background-repeat: no-repeat; background-position: center center; margin-left: auto; margin-right: auto;">
+                <div style="position: relative; width: 100%; text-align: left;">
+                    <div style="font-family: 'American Typewriter'; font-size: 1.4em;">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent laoreet odio eu dui condimentum, sed vestibulum enim dictum. Cras faucibus, dui sed ullamcorper euismod, lorem elit aliquet augue, et placerat mi metus nec justo. Aenean sit amet sagittis lacus. Etiam scelerisque ac eros non cursus. Vestibulum pharetra ipsum vel eros placerat, vel ullamcorper quam ullamcorper. Cras odio sapien, consequat sit amet mauris eu, rhoncus accumsan augue. Etiam finibus sodales tempus. Nullam lobortis tempus tellus, eu placerat velit finibus sit amet. Curabitur scelerisque nec justo in fermentum.
+                        <br /><br />
+                        In felis nisl, bibendum id dapibus in, luctus vitae ipsum. Maecenas bibendum at dolor euismod ullamcorper. Nullam consectetur metus non tortor congue, sed congue turpis interdum. Mauris elementum nunc nec odio gravida molestie. Maecenas porttitor pellentesque nibh non scelerisque. Suspendisse potenti. Praesent gravida ornare nisi, non pellentesque elit suscipit non. Mauris quis orci at nibh viverra posuere dictum dictum quam. Etiam lobortis ultricies facilisis. Quisque pellentesque lectus nulla, at vulputate nunc posuere sit amet. Curabitur vulputate sapien sed turpis pharetra efficitur. Proin nec dapibus nulla. Quisque pellentesque lectus nulla, at vulputate nunc posuere sit amet. Curabitur vulputate sapien sed turpis pharetra efficitur. Proin nec dapibus nulla.
+                    </div>
+                    <div style="clear: both;"></div>
+                </div>
+            </div>
+        </div>
+        <div style="position: relative; width: 100%; background-image: url('/images/WaveBkGrnd.gif'); background-repeat: repeat; margin-top: 7%;">
+            <div style="position: relative; width: 100%; max-width: 1336px; min-width: 336px; padding: 1.5%; background-image: url('/images/Panel1BkGrnd.gif'); background-size: 100% 100%; background-repeat: no-repeat; background-position: center center; margin-left: auto; margin-right: auto;">
+                <div style="position: relative; width: 100%; text-align: left;">
+                    <div style="font-family: 'American Typewriter'; font-size: 1.4em;">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent laoreet odio eu dui condimentum, sed vestibulum enim dictum. Cras faucibus, dui sed ullamcorper euismod, lorem elit aliquet augue, et placerat mi metus nec justo. Aenean sit amet sagittis lacus. Etiam scelerisque ac eros non cursus. Vestibulum pharetra ipsum vel eros placerat, vel ullamcorper quam ullamcorper. Cras odio sapien, consequat sit amet mauris eu, rhoncus accumsan augue. Etiam finibus sodales tempus. Nullam lobortis tempus tellus, eu placerat velit finibus sit amet. Curabitur scelerisque nec justo in fermentum.
+                        <br /><br />
+                        In felis nisl, bibendum id dapibus in, luctus vitae ipsum. Maecenas bibendum at dolor euismod ullamcorper. Nullam consectetur metus non tortor congue, sed congue turpis interdum. Mauris elementum nunc nec odio gravida molestie. Maecenas porttitor pellentesque nibh non scelerisque. Suspendisse potenti. Praesent gravida ornare nisi, non pellentesque elit suscipit non. Mauris quis orci at nibh viverra posuere dictum dictum quam. Etiam lobortis ultricies facilisis. Quisque pellentesque lectus nulla, at vulputate nunc posuere sit amet. Curabitur vulputate sapien sed turpis pharetra efficitur. Proin nec dapibus nulla. Quisque pellentesque lectus nulla, at vulputate nunc posuere sit amet. Curabitur vulputate sapien sed turpis pharetra efficitur. Proin nec dapibus nulla.
+                        <br /><br />
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent laoreet odio eu dui condimentum, sed vestibulum enim dictum. Cras faucibus, dui sed ullamcorper euismod, lorem elit aliquet augue, et placerat mi metus nec justo. Aenean sit amet sagittis lacus. Etiam scelerisque ac eros non cursus. Vestibulum pharetra ipsum vel eros placerat, vel ullamcorper quam ullamcorper. Cras odio sapien, consequat sit amet mauris eu, rhoncus accumsan augue. Etiam finibus sodales tempus. Nullam lobortis tempus tellus, eu placerat velit finibus sit amet. Curabitur scelerisque nec justo in fermentum.
+                        <br /><br />
+                        In felis nisl, bibendum id dapibus in, luctus vitae ipsum. Maecenas bibendum at dolor euismod ullamcorper. Nullam consectetur metus non tortor congue, sed congue turpis interdum. Mauris elementum nunc nec odio gravida molestie. Maecenas porttitor pellentesque nibh non scelerisque. Suspendisse potenti. Praesent gravida ornare nisi, non pellentesque elit suscipit non. Mauris quis orci at nibh viverra posuere dictum dictum quam. Etiam lobortis ultricies facilisis. Quisque pellentesque lectus nulla, at vulputate nunc posuere sit amet. Curabitur vulputate sapien sed turpis pharetra efficitur. Proin nec dapibus nulla. Quisque pellentesque lectus nulla, at vulputate nunc posuere sit amet. Curabitur vulputate sapien sed turpis pharetra efficitur. Proin nec dapibus nulla.
+                        <br /><br />
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent laoreet odio eu dui condimentum, sed vestibulum enim dictum. Cras faucibus, dui sed ullamcorper euismod, lorem elit aliquet augue, et placerat mi metus nec justo. Aenean sit amet sagittis lacus. Etiam scelerisque ac eros non cursus. Vestibulum pharetra ipsum vel eros placerat, vel ullamcorper quam ullamcorper. Cras odio sapien, consequat sit amet mauris eu, rhoncus accumsan augue. Etiam finibus sodales tempus. Nullam lobortis tempus tellus, eu placerat velit finibus sit amet. Curabitur scelerisque nec justo in fermentum.
+                        <br /><br />
+                        In felis nisl, bibendum id dapibus in, luctus vitae ipsum. Maecenas bibendum at dolor euismod ullamcorper. Nullam consectetur metus non tortor congue, sed congue turpis interdum. Mauris elementum nunc nec odio gravida molestie. Maecenas porttitor pellentesque nibh non scelerisque. Suspendisse potenti. Praesent gravida ornare nisi, non pellentesque elit suscipit non. Mauris quis orci at nibh viverra posuere dictum dictum quam. Etiam lobortis ultricies facilisis. Quisque pellentesque lectus nulla, at vulputate nunc posuere sit amet. Curabitur vulputate sapien sed turpis pharetra efficitur. Proin nec dapibus nulla. Quisque pellentesque lectus nulla, at vulputate nunc posuere sit amet. Curabitur vulputate sapien sed turpis pharetra efficitur. Proin nec dapibus nulla.
+                    </div>
+                    <div style="clear: both;"></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
